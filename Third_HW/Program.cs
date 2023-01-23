b66ac3b7f6dbbc7010c26d46138a047ab3e458da
ekaterina <whitedevil93@mail.ru>
2023-01-23 18:57:20 +0300

    thirdW

---
+++ Third_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.Write(" Введите число: ");
+int x;
+x = Console.Read();
+if ((x % 2) == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число является нечётным");
+}
