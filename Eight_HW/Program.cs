9670898ec739d11bc10b729da001753148d2997f
ekaterina <whitedevil93@mail.ru>
2023-01-23 20:28:54 +0300

    EighthW

---
+++ Eight_HW/Program.cs
@@ -0,0 +1,19 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+Console.WriteLine();
+
+int Number = new Random().Next(100, 999);
+int NewNumber = 0;
+
+int NumberWithoutSecondDigit()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumberWithoutSecondDigit();
+
+Console.WriteLine();
+Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+Console.WriteLine();
