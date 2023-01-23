d485d8bf75e57ad62d6126a651604566080d4856
ekaterina <whitedevil93@mail.ru>
2023-01-23 22:49:56 +0300

    NinthW

---
+++ Ninth_HW/Program.cs
@@ -0,0 +1,24 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Clear();
+Console.WriteLine();
+
+int Number1 = new Random().Next(50, 100);
+int Number2 = new Random().Next(1, 50);
+int result = 0;
+
+int MultiNumber()
+{
+    result = Number1 % Number2;
+    return result;
+}
+
+MultiNumber();
+
+Console.WriteLine();
+if (result == 0) Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+else
+{
+    Console.WriteLine($"Число {Number1} не кратно числу {Number2}, остаток равен {Number1 % Number2}");
+}
+Console.WriteLine();
