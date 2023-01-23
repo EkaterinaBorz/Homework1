e6afaaf95b355eedf01ed2bb60e4af8618b0e3a3
ekaterina <whitedevil93@mail.ru>
2023-01-23 18:48:30 +0300

    secondW

---
+++ Second_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+Console.Write("Введите номер дня недели:   ");
+string writeDay = Console.ReadLine();
+bool result = int.TryParse(writeDay, out int dayNumber);
+
+Console.WriteLine();
+if (result == false) Console.WriteLine("Число введено неверно");
+else if (0 < dayNumber & dayNumber < 8) Console.WriteLine($" {Days[dayNumber - 1]}");
+else Console.WriteLine(" С таким номером дня недели не существует");
+Console.WriteLine();
