e9df5882da9c311c77a83bb7a7ce5e25afb6eb35
ekaterina <whitedevil93@mail.ru>
2023-01-23 20:02:05 +0300

    seventhW

---
+++ Sevent_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear();
+
+int Number = new Random().Next(10, 99);
+int Max = 0;
+
+int MaxDigit()
+{
+    int Digit1 = Number / 10;
+    int Digit2 = Number % 10;
+    if (Digit1 > Digit2) Max = Digit1;
+    else Max = Digit2;
+
+    return Max;
+}
+
+MaxDigit();
+Console.WriteLine();
+Console.WriteLine($"В числе {Number} наибольшая цифра {Max}");
+Console.WriteLine();
