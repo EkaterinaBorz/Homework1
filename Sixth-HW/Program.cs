aebb0016ab4aa44760c1525d10cb0203128cfbf3
ekaterina <whitedevil93@mail.ru>
2023-01-23 19:50:59 +0300

    sixthW

---
+++ Sixth-HW/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+Console.WriteLine();
+
+int number = new Random().Next(100, 999);
+
+Console.WriteLine($"Вторая цифра числа {number} - это цифра {number/10 % 10}");
+Console.WriteLine();
