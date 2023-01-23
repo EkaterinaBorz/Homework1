554c0bc95239b9046065a8b850f72a7152381ce8
ekaterina <whitedevil93@mail.ru>
2023-01-23 19:39:10 +0300

    fithW

---
+++ Fifth_HW/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Clear();
+Console.WriteLine();
+
+int number = new Random().Next(100, 999);
+
+Console.WriteLine($"Последняя цифра числа {number} - это цифра {number%10}");
+Console.WriteLine();
