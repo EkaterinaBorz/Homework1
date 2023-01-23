8b7c51cd1fb661c0c9709538a017fde2109cdeff
ekaterina <whitedevil93@mail.ru>
2023-01-23 19:14:32 +0300

    fourthW

---
+++ Fourth_HW/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число:   ");
+string txtN = Console.ReadLine();
+int endNum = Convert.ToInt32(txtN);
+int num = 0;
+Console.WriteLine($"Чётные числа из диапазона от 1 до {endNum}:");
+while (num < endNum)
+{
+    num++;
+    if (num % 2 == 0) Console.Write($"{num}, ");
+}
+Console.WriteLine(); 
